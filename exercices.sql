723dbbbbe1d9f2265421db0f1761992db548e835
Andra Cioroianu <103530913+andra-c-itf@users.noreply.github.com>
2023-02-22 21:46:33 +0200

    more queries using join

--- exercices.sql
+++ exercices.sql
@@ -803,3 +803,94 @@ FROM
     departments_dup d ON m.dept_no = d.dept_no
 GROUP BY m.emp_no
 ORDER BY dept_no;
+
+SELECT 
+    d.dept_no, m.emp_no, d.dept_name
+FROM
+    departments_dup d
+        LEFT JOIN
+    dept_manager_dup m ON m.dept_no = d.dept_no
+ORDER BY d.dept_no;
+
+SELECT 
+    m.dept_no, m.emp_no, d.dept_name
+FROM
+    dept_manager_dup m
+        LEFT JOIN
+    departments_dup d ON m.dept_no = d.dept_no
+WHERE
+    dept_name IS NULL
+ORDER BY m.dept_no;
+
+#Join the 'employees' and the 'dept_manager' tables to return a subset of all the employees whose last name is Markovitch. See if the output contains a manager with that name.  
+SELECT 
+    e.emp_no,
+    e.first_name,
+    e.last_name,
+    dm.dept_no,
+    dm.from_date
+FROM
+    employees e
+        LEFT JOIN
+    dept_manager dm ON e.emp_no = dm.emp_no
+WHERE
+    e.last_name = 'Markovitch'
+ORDER BY dm.dept_no DESC , e.emp_no;
+
+#RIGHT JOIN
+SELECT 
+    d.dept_no, m.emp_no, d.dept_name
+FROM
+    dept_manager_dup m
+        RIGHT JOIN
+    departments_dup d ON m.dept_no = d.dept_no
+ORDER BY dept_no;
+
+#Using WHERE to join tables (the old join syntax)
+SELECT 
+    m.dept_no, m.emp_no, d.dept_name
+FROM
+    dept_manager_dup m,
+    departments_dup d
+WHERE
+    m.dept_no = d.dept_no
+ORDER BY m.dept_no;
+
+#Extract a list containing information about all managers’ employee number, first and last name, department number, and hire date. Use the old type of join syntax to obtain the result.
+SELECT 
+    e.emp_no, e.first_name, e.last_name, dm.dept_no, e.hire_date
+FROM
+    employees e,
+    dept_manager dm
+WHERE
+    e.emp_no = dm.emp_no;
+
+
+SELECT 
+    e.emp_no, e.first_name, e.last_name, dm.dept_no, e.hire_date
+FROM
+    employees e
+        JOIN
+    dept_manager dm ON e.emp_no = dm.emp_no; 
+    
+#Using JOIN and WHERE in the same query
+SELECT 
+    e.emp_no, e.first_name, e.last_name, s.salary
+FROM
+    employees e
+        JOIN
+    salaries s ON e.emp_no = s.emp_no
+WHERE
+    s.salary > 145000;
+    
+#Select the first and last name, the hire date, and the job title of all employees whose first name is “Margareta” and have the last name “Markovitch”.
+SELECT 
+    e.first_name, e.last_name, e.hire_date, t.title
+FROM
+    employees e
+        JOIN
+    titles t ON e.emp_no = t.emp_no
+WHERE
+    first_name = 'Margareta'
+        AND last_name = 'Markovitch'
+ORDER BY e.emp_no;   
